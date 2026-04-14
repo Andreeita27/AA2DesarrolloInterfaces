dcc72b08fdc18e630927d21d1205da91fe3c15c6
Andrea Fernandez de la Rosa <a28707@svalero.com>
2026-04-14 18:32:51 +0200

    Añado aclaraciones al código

--- frontend/src/types/auth.ts
+++ frontend/src/types/auth.ts
@@ -1,23 +1,28 @@
-export type UserRole = 'ADMIN' | 'CLIENT';
+export type UserRole = 'ADMIN' | 'CLIENT'; // Roles posibles que puede devolver el backend
 
-export interface User {
+// Esto representa al usuario autenticado
+// Esta info se guarda en contexto y en localstorage
+export interface AuthUser {
     id: number;
     name: string;
     email: string;
     role: UserRole;
 }
 
+//Estos son los datos que se envian al backend para inciar sesión
 export interface LoginRequest {
     email: string;
     password: string;
 }
 
+//Datos que se envían al backend para registrar un nuevo usuario
 export interface RegisterRequest {
     name: string;
     email: string;
     password: string;
 }
 
+//Respuesta esperada del backend tras login o registro
 export interface AuthResponse {
     token: string;
     user: AuthUser;
